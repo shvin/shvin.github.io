b9a1924c6113ab700717424ab386935b0850ace6
ashvin <ashvin0119@gmail.com>
2021-10-27 18:56:21 -0400

    updated readme, styled navbar

--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -1,43 +1,93 @@
 import styled from 'styled-components'
 import { Link as LinkRouter} from 'react-router-dom'
-// import { Link as LinkScroll} from 'react-scroll'
+import { Link as LinkScroll} from 'react-scroll'
+
+
+// margin-top: -80px; (for the navbar)
 
 export const Nav = styled.nav`
-    background: #000;
-    height: 80px;
-    margin-top: -80px;
+    background: #282828;
+    height: 60px;
     position: sticky;
     font-size: 1rem;
     display: flex;
     justify-content: center;
     align-items: center;
     z-index: 10;
-    top: 0;
 
     @media screen and (max-width: 960px) {
         transition: 0.8s all ease;
     }
-`
+`;
 
 export const NavbarContainer = styled.div`
     display: flex;
     justify-content: space-between;
-    height: 80px;
+    height: 60px;
     z-index: 1;
     width: 100%;
     padding: 0 24px;
     max-width: 1100px;
-`
+`;
 
 export const NavbarLogo = styled(LinkRouter)`
-    color: #fff;
     justify-self: flex-start;
     cursor: pointer;
-    font-size: 1.5rem;
     dispaly: flex;
     align-items: center;
-    margin-left: 24px;
+    margin-left: 12px;
     font-weight: bold;
     text-decoration: none;
     transition: 0.8s all ease;
+    font-family: 'Gloria Hallelujah', cursive;
+    font-weight: 700;
+    font-style: italic;
+    font-size: 2rem;
+    color: #3500d3;
+`;
+
+export const MobileIcon = styled.div`
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+        position: absolute;
+        top: 12%;
+        right: 3%;
+        font-size: 1.8rem;
+        cursor: pointer;
+        color: #fff;
+    }
+`
+
+export const NavMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+    margin-right: -22px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+export const NavItem = styled.li`
+    height: 80px;
+    padding-top: 20px;
+`;
+
+export const NavLink = styled(LinkScroll)`
+    color: #fff;
+    text-decoration: none;
+    font-size: 1.5rem;
+    margin-right: 22px;
+    transition: 0.8s all ease;
+    height: 100%;
+    cursor: pointer;
+    padding: 0 1 rem;
+
+    &.active {
+        border-bottom: 3px solid #3500D3;
+    }
 `
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavbarLogo } from './NavbarElements';
+import { FaBars } from 'react-icons/fa';
+import { 
+    Nav,
+    NavbarContainer,
+    NavbarLogo,
+    MobileIcon,
+    NavMenu, NavItem,
+    NavLink
+    } from './NavbarElements';
 
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavbarLogo>
-                        ashvin
-                    </NavbarLogo>
+                    <NavbarLogo to="/">ASHVIN</NavbarLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLink to="about">
+                                about
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="projects">
+                                projects
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="contact">
+                                contact
+                            </NavLink>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </>
